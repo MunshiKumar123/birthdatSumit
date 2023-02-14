6e472679dacb57f252ae25a5153a541e22e446f7
Munshi Kumar <munshitesting101@gmail.com>
2023-02-14 21:30:44 +0530

    birthdat

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Sumit from './component/Sumit';
+const App = () => {
+  return (
+   <>
+    <Sumit />
+   </>
+  )
+}
+
+export default App;
---
+++ src/component/Sumit.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Sumit = () => {
+
+  return (
+   <>
+
+<h2>Touch on Card</h2>
+<div className="body">
+ <div className="birthdayCard">
+<div className="cardFront"><h3 className="happy">HAPPY BIRTHDAY!<br/> Sumit Singh</h3>
+<div className="balloons">
+  <div className="balloonOne"></div>
+  <div className="balloonTwo"></div>
+  <div className="balloonThree"></div>
+  <div className="balloonFour"></div>
+</div>
+
+</div>
+<div className="cardInside">
+    <h3 className="back">HAPPY BIRTHDAY!</h3>
+    <p>Dear Best Friend,</p> 
+    <p>Happy birthday!! I hope your day is filled with lots of love and laughter! May all of your birthday wishes come true.</p>
+    <p className="name">🎂Sumit Singh Rajput🎂</p>
+  </div>
+  <h2>From👉Kamal</h2>
+</div>
+</div>
+
+
+
+   </>
+  )
+}
+
+export default Sumit;
